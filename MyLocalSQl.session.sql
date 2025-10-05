9585c6cc22bc65c67ecd07d9339a3d0c6318bdca
Sai Jetendra <saijithendra807@gmail.com>
2025-10-05 11:33:33 +0530

    STUDENT TABLE

---
+++ MyLocalSQl.session.sql
@@ -0,0 +1,6 @@
+CREATE TABLE Student(
+Stduent_Name INT,
+Student_ID VARCHAR(10),
+Student_email VARCHAR(20)
+);
+SELECT*FROM student;
--- MySql/first.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE Stduents_Detalis(
-    student_Name VARCHAR(20),
-    student_ID VARCHAR(10),
-    student_email VARCHAR(20)
-
-);
